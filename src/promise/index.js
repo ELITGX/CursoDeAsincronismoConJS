639f9d59829b616cd6fe25a2eab13807c49f23f8
ELITGX <elitgx@gmail.com>
2021-05-14 16:11:09 -0500

    Terminando curso de asincronismo con JavaScript

--- src/promise/index.js
+++ src/promise/index.js
@@ -3,9 +3,37 @@ const somethingWillHappen = () => {
         if (true) {
             resolve('Hey!');
         } else {
-            reject('Whoops');
+            reject('Whoops!');
         }
     })
 }
 
 somethingWillHappen()
+ .then(response => console.log(response))
+ .catch(err => console.log(err));
+
+ const somethingWillHappen2 = () => {
+     return new Promise((resolve, reject) => {
+         if (true) {
+             setTimeout(() => {
+                 resolve('true');
+             }, 2000)
+         } else {
+             const error = new Error('Whoops!');
+             reject(error);
+         }
+     });
+ }
+
+ somethingWillHappen2()
+ .then(response => console.log(response))
+ .catch(err => console.error(err));
+
+
+ Promise.all([somethingWillHappen2(), somethingWillHappen()])
+ .then(response => {
+     console.log('Array of results', response);
+ })
+.catch(err => {
+    console.error(err);
+})
